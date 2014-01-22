6bba45bc7146cba4e22e6291cce82bfb5bb17969
Joe Cochran <j_cochran@me.com>
2014-01-22 07:48:54 -0800

    layout updates

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -8,6 +8,7 @@
 <p>{{ link_to_route('pages.create', 'Add new page') }}</p>
 
 @if ($pages->count())
+<div class="panel panel-default">
 	<table class="table table-striped">
 		<thead>
 			<tr>
@@ -21,7 +22,7 @@
 				<tr>
 					<td>{{{ $page->title }}}</td>
 					<td>{{{ $page->description }}}</td>
-          <td>{{ link_to_route('pages.edit', 'Edit', array($page->id), array('class' => 'btn btn-info')) }}</td>
+          <td>{{ link_to_route('pages.edit', 'Edit', array($page->id), array('class' => 'btn btn-info pull-right')) }}</td>
           {{-- <td>
                         {{ Form::open(array('method' => 'DELETE', 'route' => array('pages.destroy', $page->id))) }}
                             {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
@@ -31,6 +32,7 @@
 			@endforeach
 		</tbody>
 	</table>
+</div>
 @else
 	There are no pages
 @endif
