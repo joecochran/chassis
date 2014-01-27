b07cb3891fbb6077440e610e9db851386c1b3dd3
Joe Cochran <j_cochran@me.com>
2014-01-27 07:50:45 -0800

    switching login to username

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -9,15 +9,15 @@ class SessionsController extends BaseController {
 
 	public function store()
 	{
-        // validate
+        // validate before attempt guy
+
 		$input = Input::all();
         $attempt = Auth::attempt(array(
-            'email' => $input['email'],
+            'username' => $input['username'],
             'password' => $input['password']
         ));
         if ($attempt) return Redirect::intended('admin')->with(['flash_message'=>'Login Successful', 'message_class'=>'alert-success','message_strong'=>'Welcome!']);
         return Redirect::back()->with(['flash_message'=>'Your username or password is incorrect.','message_class'=>'alert-danger','message_strong'=>'Uh oh!'])->withInput();
-
 	}
 
 	public function destroy()
