6237b1b63f6052f7c76b7f8c91280ed9d2950f8e
Joe Cochran <j_cochran@me.com>
2014-01-26 22:47:20 -0800

    brought in gulpjs for asset management

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/assets/js/vendor/jquery-1.11.0.min.js',
+        'app/assets/js/vendor/semantic.js',
+        'app/assets/js/scripts.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'scripts task complete' }));
+});
+gulp.task('styles', function(){
+    return gulp.src('app/assets/scss/main.scss')
+        .pipe(sass({ style: 'expanded'}))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css/'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'styles task complete'}));
+});
+gulp.task('images', function() {
+    return gulp.src('assets/img/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(livereload(server))
+        .pipe(gulp.dest('public/img'))
+        .pipe(notify({ message: 'images task complete' }));
+});
+
+
+gulp.task('watch', function () {
+    server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err)
+        };
+        gulp.watch('app/assets/scss/**/*.scss', ['styles']);
+        gulp.watch('app/assets/js/**/*.js', ['scripts']);
+        gulp.watch('app/assets/img/**/*', ['images']);
+    });
+});
+
+
+gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
