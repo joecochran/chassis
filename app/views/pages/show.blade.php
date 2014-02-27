c6307022101b8894256c84c3b1cd7a941e92522c
Joe Cochran <j_cochran@me.com>
2014-02-26 18:10:58 -0800

    bringing back semantic ui

--- app/views/pages/show.blade.php
+++ app/views/pages/show.blade.php
@@ -7,7 +7,7 @@
     <img src="http://placehold.it/1680x500" class="banner-image" alt="" />
 </div>
 
-<article>
+<article class="ui segment">
 <h1>{{{ $page->title }}}</h1>
 {{ $page->content }}
 </article>
