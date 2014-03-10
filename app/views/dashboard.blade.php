ed449b83da6a6d3b04b89a981a38f9b0beddeac5
Joe Cochran <j_cochran@me.com>
2014-03-10 05:42:06 -0700

    quick dash update

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     Changelog:
                 </div>
                 <div class="extra text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Stabilized current featureset and removed scaffolded / boilerplate stuff to clear the way for future features.</p>
                 </div>
             </div>
         </div>
