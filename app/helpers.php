7cffcc6fe9422181e748ab670da8491ebfa59054
Joe Cochran <j_cochran@me.com>
2014-03-19 12:28:41 -0700

    some cleanup

--- app/helpers.php
+++ app/helpers.php
@@ -6,7 +6,7 @@
  */
 function set_active($path, $active = ' active')
 {
-    return Request::is($path) ? $active : '';
+    return Request::is($path) || Request::is($path.'/*') ? $active : '';
 }
 
 /**
