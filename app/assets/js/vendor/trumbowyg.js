456316295a8f5ab8521c4f785701b83610c4afe9
shaynestatzell <sstatzell@gmail.com>
2014-05-08 09:19:12 -0700

    Update trumbowyg.js
    Updated trumbowyg, fixes semantic bug where it was adding <p> tags every 2-3 characters.

--- app/assets/js/vendor/trumbowyg.js
+++ app/assets/js/vendor/trumbowyg.js
@@ -1,4 +1,4 @@
-﻿/* ===========================================================
+/* ===========================================================
  * trumbowyg.js
  * Core code of Trumbowyg plugin
  * http://alex-d.github.com/Trumbowyg
@@ -104,6 +104,10 @@ $.trumbowyg = {
                     // Destroy
                     case 'destroy':
                         return t.destroy();
+
+                    // Empty
+                    case 'empty':
+                        return t.empty();
 
                     // Public options
                     case 'lang':
@@ -377,8 +381,8 @@ $.trumbowyg = {
                 });
                 return false;
             })
-            .on('keyup', function(){
-                that.semanticCode();
+            .on('keyup', function(e){
+                that.semanticCode(false, e.which === 13);
             })
             .on('blur', function(){
                 that.syncCode();
@@ -658,9 +662,17 @@ $.trumbowyg = {
 
 
 
+        // Empty the editor
+        empty: function(){
+            this.$e.val('');
+            this.syncCode(true);
+        },
+
+
+
         // Function call when click on viewHTML button
         toggle: function(){
-            this.semanticCode();
+            this.semanticCode(false, true);
             this.$editor.toggle();
             this.$e.toggle();
             this.$btnPane.toggleClass(this.o.prefix + 'disable');
@@ -719,7 +731,9 @@ $.trumbowyg = {
         },
 
         // Analyse and update to semantic code
-        semanticCode: function(force){
+        // @param force : force to sync code from textarea
+        // @param full  : wrap text nodes in <p>
+        semanticCode: function(force, full){
             this.syncCode(force);
 
             if(this.o.semantic){
@@ -727,15 +741,21 @@ $.trumbowyg = {
                 this.semanticTag('i', 'em');
                 this.semanticTag('strike', 'del');
 
-                // Wrap text nodes in p
-                this.$editor.contents()
-                .filter(function(){
-                    // Only non-empty text nodes
-                    return this.nodeType === 3 && $.trim(this.nodeValue).length > 0;
-                }).wrap('<p></p>').end()
+                if(full){
+                    // Wrap text nodes in p
+                    this.$editor.contents()
+                    .filter(function(){
+                        // Only non-empty text nodes
+                        return this.nodeType === 3 && $.trim(this.nodeValue).length > 0;
+                    }).wrap('<p></p>').end()
 
-                // Remove all br
-                .filter("br").remove();
+                    // Remove all br
+                    .filter("br").remove();
+
+                    this.saveSelection();
+                    this.semanticTag('div', 'p');
+                    this.restoreSelection();
+                }
 
                 this.$e.val(this.$editor.html());
             }
