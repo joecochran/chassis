9386889d6887e62a67cba702decf6d584aa9222f
Joe Cochran <joe@cochran.io>
2014-05-15 10:55:34 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    minifycss = require('gulp-minify-css'),
+    minifycss = require('gulp-cssmin'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
     compass = require('gulp-compass'),
@@ -8,10 +8,9 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
+    cache = require('gulp-cache')
+    cmq = require('gulp-combine-media-queries'),
+    svgmin = require('gulp-svgmin'),;
 
 gulp.task('scripts', function() {
     return gulp.src([
@@ -27,9 +26,7 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('public/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('public/js'))
-        .pipe(livereload(server))
-        .pipe(notify({ message: 'scripts task complete' }));
+        .pipe(gulp.dest('public/js'));
 });
 gulp.task('styles', function(){
     return gulp.src('app/assets/scss/main.scss')
@@ -42,31 +39,26 @@ gulp.task('styles', function(){
         }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('public/css'))
+        .pipe(cmq())
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('public/css/'))
-        .pipe(livereload(server))
-        .pipe(notify({ message: 'styles task complete'}));
+        .pipe(gulp.dest('public/css/'));
 });
 gulp.task('images', function() {
     return gulp.src('app/assets/img/**/*')
         .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(livereload(server))
-        .pipe(gulp.dest('public/img'))
-        .pipe(notify({ message: 'images task complete' }));
+        .pipe(gulp.dest('public/img'));
 });
-
+gulp.task('svg', function(){
+    return gulp.src('app/assets/svg/**/*')
+        .pipe(cache(svgmin()))
+        .pipe(gulp.dest('public/svg/'));
+});
 
 gulp.task('watch', function () {
-    server.listen(35729, function (err) {
-        if (err) {
-            return console.log(err)
-        };
-        gulp.watch('app/assets/scss/**/*.scss', ['styles']);
-        gulp.watch('app/assets/js/**/*.js', ['scripts']);
-        gulp.watch('app/assets/img/**/*', ['images']);
-    });
+    gulp.watch('app/assets/scss/**/*.scss', ['styles']);
+    gulp.watch('app/assets/js/**/*.js', ['scripts']);
 });
 
 
-gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
